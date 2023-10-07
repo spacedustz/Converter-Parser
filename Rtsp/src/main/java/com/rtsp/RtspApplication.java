4adab9d66710205eb34385a3b0df234ef93a1f75
spacedustz <tensorflow555@gmail.com>
2023-10-07 19:15:14 +0900

    Add RTSP Project

---
+++ Rtsp/src/main/java/com/rtsp/RtspApplication.java
@@ -0,0 +1,13 @@
+package com.rtsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtspApplication.class, args);
+    }
+
+}
