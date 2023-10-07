c3f83b67057fc05be059fc3f36c0ad9d1695e18d
spacedustz <tensorflow555@gmail.com>
2023-10-08 05:26:20 +0900

    Main App - AppBlock 추가

--- Viewer/src/App.tsx
+++ Viewer/src/App.tsx
@@ -1,5 +1,21 @@
 import './App.css'
 
+import styled, {css} from "styled-components";
 
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const App: React.FC = () => {
+    <div>
+        <AppBlock>
+
+        </AppBlock>
+    </div>
+}
 
 export default App
