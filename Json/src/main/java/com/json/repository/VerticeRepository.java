a3c3e2d269955f171fab0ab75b6a8f93474a5cf9
spacedustz <tensorflow555@gmail.com>
2023-10-08 04:00:38 +0900

    Repository 작성

---
+++ Json/src/main/java/com/json/repository/VerticeRepository.java
@@ -0,0 +1,7 @@
+package com.json.repository;
+
+import com.json.entity.Vertice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerticeRepository extends JpaRepository<Vertice, Long> {
+}
