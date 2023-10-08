dc51b5caad0b6cb29cfd6af128aa6eccaa8ae8e6
spacedustz <spacedustw@gmail.com>
2023-10-09 01:32:44 +0900

    Cors Config 변경

--- Rtsp/src/main/java/com/rtsp/streaming/CorsConfig.java
+++ Rtsp/src/main/java/com/rtsp/streaming/CorsConfig.java
@@ -2,7 +2,6 @@ package com.rtsp.streaming;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
-                .allowedOrigins("localhost:3000", "localhost:5002") // 허용할 origin
+        registry.addMapping("/rtsp")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5002") // 허용할 origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // Credentials (쿠키 등) 허용 여부
