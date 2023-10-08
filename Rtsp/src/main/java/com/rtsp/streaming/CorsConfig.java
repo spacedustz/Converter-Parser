ed0b60eed9376f868171c0923cfa97677b30391a
spacedustz <spacedustw@gmail.com>
2023-10-09 01:20:37 +0900

    Cors Config 변경

--- Rtsp/src/main/java/com/rtsp/streaming/CorsConfig.java
+++ Rtsp/src/main/java/com/rtsp/streaming/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("*")
-                .allowedOrigins("localhost:3000", "15.164.220.68:5002", "localhost:5002") // 허용할 origin
+                .allowedOrigins("localhost:3000", "localhost:5002") // 허용할 origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // Credentials (쿠키 등) 허용 여부
