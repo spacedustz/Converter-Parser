f6bc36635feef71cfa440ff832582c04ea794c99
spacedustz <tensorflow555@gmail.com>
2023-10-08 13:37:02 +0900

    Streaming DTO 작성

--- Rtsp/src/main/java/com/rtsp/streaming/StreamingDto.java
+++ Rtsp/src/main/java/com/rtsp/streaming/StreamingDto.java
@@ -1,2 +1,16 @@
-package com.rtsp.streaming;public class StreamingDto {
+package com.rtsp.streaming;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class StreamingDto {
+    private String url;
+
+    public static StreamingDto mapping(String url) {
+        return new StreamingDto(url);
+    }
 }
