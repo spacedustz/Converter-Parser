06883d67ac5913ce57eaf6117c8dd8f86bebafaf
spacedustz <tensorflow555@gmail.com>
2023-10-08 13:36:55 +0900

    Streaming Controller 작성

--- Rtsp/src/main/java/com/rtsp/streaming/StreamingController.java
+++ Rtsp/src/main/java/com/rtsp/streaming/StreamingController.java
@@ -1,2 +1,25 @@
-package com.rtsp.streaming;public class StreamingController {
+package com.rtsp.streaming;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rtsp")
+@RequiredArgsConstructor
+public class StreamingController {
+    private final StreamingService streamingService;
+
+    @GetMapping("/linux")
+    public ResponseEntity<StreamingDto> rtspToHlsOnLinux() {
+        return new ResponseEntity<>(StreamingDto.mapping(streamingService.convertToHlsOnLinux()), HttpStatus.OK);
+    }
+
+    @GetMapping("/windows")
+    public ResponseEntity<StreamingDto> rtspToHlsOnWindows() {
+        return new ResponseEntity<>(StreamingDto.mapping(streamingService.convertToHlsOnLinux()), HttpStatus.OK);
+    }
 }
